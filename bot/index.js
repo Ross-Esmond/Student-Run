5f2a699631c0afdf2a2fc5872f20de2e815a0d36
Ross Esmond <r.esmond@protonmail.com>
2022-05-22 10:49:15 -0500

    feat: combined discord bot server and next js server

--- bot/index.js
+++ bot/index.js
@@ -34,6 +34,34 @@ const logger = createLogger({
     ]
 })
 
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const hostname = 'localhost'
+const port = 3000
+const app = next({ dev, hostname, port })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+    createServer(async (req, res) => {
+        try {
+            const parsedUrl = parse(req.url, true)
+            const { pathname, query } = parsedUrl
+
+            await handle(req, res, parsedUrl)
+        } catch (err) {
+            logger.error('Error occurred handling', req.url, err)
+            res.statusCode = 500
+            res.end('internal server error')
+        }
+    }).listen(port, (err) => {
+        if (err) throw err
+        logger.info(`> Ready on http://${hostname}:${port}`)
+    })
+})
+
 const classRegex = /^[a-zA-Z]{2,4}-\d{4}(H|W|h|w)?$/
 
 const interactionStore = new Map()
